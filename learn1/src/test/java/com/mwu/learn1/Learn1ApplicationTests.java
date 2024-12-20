f814dcc1b6474e418689b9b17aedb9750d823ffa
wudye <mingwei.wu@hotmail.com>
2024-12-20 09:32:10 +0100

    first project

---
+++ learn1/src/test/java/com/mwu/learn1/Learn1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwu.learn1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learn1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
